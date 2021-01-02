80a069581c7954fa9b1fb4154be00bfa23e4633d
yujin779 <51077021+yujin779@users.noreply.github.com>
2021-01-03 05:39:57 +0900

    for分をmapに変更

--- src/components/Enemy.js
+++ src/components/Enemy.js
@@ -16,16 +16,17 @@ export const createEnemysList = (number, startX) => {
   // const startX = 15;
   // const number = 10;
   for (let i = 0; i < number; i++) {
-    let p = startX + 15;
-    if (i !== 0) p = enemysList[i - 1].position.x + Math.random() * 10 + 15;
+    let p = startX;
+    if (i !== 0) p = enemysList[i - 1].position.x + 5;
 
     enemysList.push({ position: { x: p, y: 1, z: 0 } });
   }
+  // console.log("enemysList", enemysList);
   return enemysList;
 };
 
 const Enemy = ({ value }) => {
-  console.log("group", value);
+  // console.log("group", value);
   // 物理演算させるボックスのサイズ
   const args = [2, 2, 1];
   const physicsBox = {
@@ -36,21 +37,14 @@ const Enemy = ({ value }) => {
   };
   const [ref, api] = useBox(() => physicsBox);
   useFrame(() => {
-    api.position.set(
-      value[0].position.x,
-      value[0].position.y,
-      value[0].position.z
-    );
+    api.position.set(value[0].position.x, 0, 0);
   });
   return (
     <group ref={ref}>
-      {value.map((value) => (
+      {value.map((value, index) => (
         <mesh
-          position={[
-            value.position.x,
-            value.position.y,
-            value.position.z
-          ]}
+          position={[value.position.x, value.position.y, value.position.z]}
+          key={index}
         >
           <boxBufferGeometry attach="geometry" args={args} />
           <meshStandardMaterial
@@ -69,26 +63,32 @@ const Enemy = ({ value }) => {
  * 接触するとgameover
  */
 export const EnemyData = ({ number }) => {
+  // この値になったら位置を再設定
+  const returnX = -50;
+  const startX = 10;
   // const positionCreate =
-  const [groupA, setGroupA] = useState(createEnemysList(number, 15));
+  const [groupA, setGroupA] = useState(createEnemysList(number, startX));
+  console.log("gp", groupA[groupA.length - 1].position.x);
+  const [groupB, setGroupB] = useState(
+    createEnemysList(number, groupA[groupA.length - 1].position.x)
+  );
   // const [obj2, setObj2] = useState(
   //   createEnemysList(number, obj[obj.length - 1].position[0])
   // );
   // const? obj = createEnemysList(3, 1);
   // const [landing, setLanding] = useState(false);
   useFrame(() => {
-    for (let i = 0; i < number * 2; i++) {
-      if (i < number) {
-        groupA[i].position.x -= speed;
-      } else {
-        // api.at(i).position.set(...obj2[i - number].position);
-        // obj2[i - number].position[0] -= speed;
-      }
-    }
+    groupA.map((p) => (p.position.x -= speed));
+    groupB.map((p) => (p.position.x -= speed));
 
-    if (groupA[groupA.length - 1].position.x < -14) {
-      setGroupA(createEnemysList(number, 15));
+    if (groupA[groupA.length - 1].position.x < returnX) {
+      console.log("b", groupB[groupB.length - 1].position.x);
+      setGroupA(createEnemysList(number, groupB[groupB.length - 1].position.x));
+      console.log("a", groupA[0].position.x);
     }
+    // if (groupB[groupB.length - 1].position.x < returnX) {
+    //   setGroupB(createEnemysList(number, groupA[groupA.length - 1].position.x));
+    // }
 
     // console.log(groupA[groupA.length - 1].position.x);
     // const p = obj[obj.length - 1].position[0];
@@ -99,6 +99,7 @@ export const EnemyData = ({ number }) => {
   return (
     <group>
       <Enemy value={groupA} />
+      <Enemy value={groupB} />
     </group>
   );
 };
